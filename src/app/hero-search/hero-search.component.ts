4196954446dde83c458f1c22535b3d8c150c8e86
Vicent B. F <vicentbotella.93@gmail.com>
2022-08-22 16:03:06 +0200

    Update hero-search.component.ts

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
-import {
-   debounceTime, distinctUntilChanged, switchMap
- } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 import { Hero } from '../interfaces/hero';
 import { HeroService } from '../services/hero.service';
 
@@ -14,25 +12,40 @@ import { HeroService } from '../services/hero.service';
 export class HeroSearchComponent implements OnInit {
 
   heroes$!: Observable<Hero[]>;
+  
   private searchTerms = new Subject<string>();
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService) { }
 
-  // Push a search term into the observable stream.
-  search(term: string): void {
-    this.searchTerms.next(term);
-  }
-
+  /**
+   * The function assigns to the heroes$ array the data obtained by calling the searchHeroes function of the hero service (all heroes that match the 
+   * user's search - all heroes whose names contain part of or are the same as the search term).
+   */
   ngOnInit(): void {
+    
     this.heroes$ = this.searchTerms.pipe(
-      // wait 300ms after each keystroke before considering the term
+    
+      //Waits 300ms after each keystroke before considering the search term.
       debounceTime(300),
 
-      // ignore new term if same as previous term
+      //Ignores the new search term if it's the same as the previous one.
       distinctUntilChanged(),
 
-      // switch to new search observable each time the term changes
+      //Switches to new search observable each time the search term changes.
       switchMap((term: string) => this.heroService.searchHeroes(term)),
+        
     );
+
   }
+  
+  /**
+   * Pushes the passed-in search term into the observable stream.
+   * @param term - This is the passed-in search term.
+   */
+  search(term: string): void {
+    
+    this.searchTerms.next(term);
+
+  }
+  
 }
