f93db363a998bbf4322d84e79a50c2c2a8f935a9
Vicent B. F <vicentbotella.93@gmail.com>
2022-08-22 14:47:47 +0200

    Update hero.service.ts

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -19,87 +19,139 @@ export class HeroService {
 
   constructor(private http: HttpClient, private messageService: MessageService) { }
 
+  /**
+   * GET METHOD: fetches all heroes from server. If the observable stream fails, the catchError operator intercepts the error and passes it to the 
+   * error handling function, which reports the error and returns an innocuous result so that the application can keep working (this error handling 
+   * procedure is used in all methods of this service.).
+   */
   getHeroes(): Observable<Hero[]> {
 
-    //The catchError() operator intercepts an Observable that failed. The operator then passes the error to the error handling function.
-    //The following handleError() method reports the error and then returns an innocuous result so that the application keeps working.
-    return this.http.get<Hero[]>(this.heroesUrl).pipe(tap(_ => this.log('Heroes successfully fetched!')), catchError(this.handleError<Hero[]>('getHeroes', [])));
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+      
+        tap(_ => this.log('Heroes successfully fetched!')), 
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      
+      );
 
   }
 
-  /** GET hero by id. Will 404 if id not found */
+  /** 
+   * GET BY ID METHOD: does exactly the same as the previous method, but in this case just fetches one hero (the hero that corresponds to the 
+   * passed-in id).
+   * @param id - This is the passed-in id, which corresponds to the to-be-fetched hero.
+   */  
   getHero(id: number): Observable<Hero> {
-    const url = `${this.heroesUrl}/${id}`;
-    return this.http.get<Hero>(url).pipe(
-      tap(_ => this.log(`You selected hero with id ${id}.`)),
-      catchError(this.handleError<Hero>(`getHero with id ${id}.`))
-    );
+    
+    let url = `${this.heroesUrl}/${id}`;
+    
+    return this.http.get<Hero>(url)
+      .pipe(
+      
+        tap(_ => this.log(`You selected hero with id ${id}.`)),
+        catchError(this.handleError<Hero>(`getHero with id ${id}.`))
+      
+      );
+    
   }
 
-  /** PUT: update the hero on the server */
+  /** 
+   * PUT METHOD: updates the passed-in hero on the server.
+   * @param hero - This is the passed-in hero.
+   */
   updateHero(hero: Hero): Observable<any> {
-    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
-      tap(_ => this.log(`You updated hero with id ${hero.id}.`)),
-      catchError(this.handleError<any>('updateHero'))
-    );
+    
+    return this.http.put(this.heroesUrl, hero, this.httpOptions)
+      .pipe(
+      
+        tap(_ => this.log(`You updated hero with id ${hero.id}.`)),
+        catchError(this.handleError<any>('updateHero'))
+      
+      );
+    
   }
 
-  /** POST: add a new hero to the server */
+  /** 
+   * POST METHOD: adds the passed-in hero to the server.
+   * @param hero - This is the passed-in hero.
+   */
   addHero(hero: Hero): Observable<Hero> {
-    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
-      tap((newHero: Hero) => this.log(`New hero created! Hero's name: ${newHero.name}, Hero's id: ${newHero.id}.`)),
-      catchError(this.handleError<Hero>('addHero'))
-    );
+    
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions)
+      .pipe(
+      
+        tap((newHero: Hero) => this.log(`New hero created! Hero's name: ${newHero.name}, Hero's id: ${newHero.id}.`)),
+        catchError(this.handleError<Hero>('addHero'))
+      
+      );
+    
   }
 
-  /** DELETE: delete the hero from the server */
+  /** 
+   * DELETE METHOD: deletes the hero that corresponds to the passed-in id.
+   * @param id - This is the passed-in id, which corresponds to the to-be-deleted hero.
+   */
   deleteHero(id: number): Observable<Hero> {
-    const url = `${this.heroesUrl}/${id}`;
+    
+    let url = `${this.heroesUrl}/${id}`;
 
     return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      
       tap(_ => this.log(`You deleted hero with id ${id}.`)),
       catchError(this.handleError<Hero>('deleteHero'))
+      
     );
-  }
-
-  /* GET heroes whose name contains search term */
-  searchHeroes(term: string): Observable<Hero[]> {
-    if (!term.trim()) {
-      // if not search term, return empty hero array.
-      return of([]);
-    }
-    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
-      tap(x => x.length ?
-        this.log(`found heroes matching "${term}"`) :
-        this.log(`no heroes matching "${term}"`)),
-      catchError(this.handleError<Hero[]>('searchHeroes', []))
-    );
+    
   }
 
   /**
- * Handle Http operation that failed.
- * Let the app continue.
- *
- * @param operation - name of the operation that failed
- * @param result - optional value to return as the observable result
- */
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
+   * GET BY SEARCH METHOD: does exactly the same as the "GET BY ID" method, but in this case a search term is passed in instead of an id. 
+   * @param term - This is the passed-in term, which could correspond to one, several or no hero at all.
+   */
+  searchHeroes(term: string): Observable<Hero[]> {
+    
+    if (!term.trim()) {
+      
+      return of([]);
+      
+    }
+    
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`)
+      .pipe(
+      
+        tap(x => x.length ? this.log(`Found heroes matching "${term}".`) : this.log(`No heroes found matching "${term}".`)),
+        catchError(this.handleError<Hero[]>('searchHeroes', []))
+      
+      );
+    
   }
 
-  /** Log a HeroService message with the MessageService */
+  /**
+   * Handles http operations that fail.
+   * @param operation - This is the name of the operation that failed.
+   * @param result - This is the optional value to return as the observable result.
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    
+    return (error: any): Observable<T> => {
+
+      console.error(error);
+
+      this.log(`${operation} failed: ${error.message}.`);
+
+      return of(result as T);
+      
+    };
+    
+  }
+
+  /** 
+   * Logs a HeroService message making use of the the MessageService.
+   */
   private log(message: string) {
+    
     this.messageService.add(`HeroService: ${message}`);
+    
   }
 
 }
