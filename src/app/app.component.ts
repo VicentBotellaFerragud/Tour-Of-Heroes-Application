a01a1fd14c3d447819b1134f29a371e3855ffe0a
Vicent B. F <vicentbotella.93@gmail.com>
2022-08-22 15:26:12 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,14 +12,22 @@ export class AppComponent implements AfterContentChecked {
 
   url!: string;
 
-  constructor (private router: Router) {}
+  constructor (private router: Router) { }
 
+  /**
+   * Gives the local variable "url" the value of the current url once the component has been initiated.
+   */
   ngAfterContentChecked(): void {
     
     this.url = this.router.url;
 
   }
 
+  /**
+   * Checks if the passed-in url corresponds to the current url.
+   * @param url - This is the passed-in url.
+   * @returns - true or false, depending on wehter the passed-in url corresponds to the current url or not. 
+   */
   isTheCurrentLocation(url: string) {
 
     return this.url === url ? true : false;
